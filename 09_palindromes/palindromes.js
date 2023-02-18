eb42daa225cfc1f30c0e3deea7723c49abacc766
Josh <118862870+Catch2two@users.noreply.github.com>
2023-02-18 13:18:44 -0500

    Check Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+function palindromes(string) {
 
-};
+    var replaceWithSpaces = /[\W_]/g;
+
+    var lowRegStr = string
+    .toLowerCase()
+    .replace(replaceWithSpaces, '');
+
+    var reverseStr = lowRegStr
+    .split('')
+    .reverse()
+    .join('');
+    
+    return reverseStr === lowRegStr;
+ }
 
 // Do not edit below this line
 module.exports = palindromes;
