7f126e90ac150043d0a624625f6bfcfdb2ddb87e
Josh <118862870+Catch2two@users.noreply.github.com>
2023-02-20 12:02:55 -0500

    Update calculator.js
    Added Power Function
    Added Factorial Function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,12 +24,27 @@ const multiply = function(array) {
     return sum;
 };
 
-const power = function() {
-	
-};
+const power = function(base, exponent) {
+  var result = 1;
+  if(exponent === undefined) {
+    exponent = 2;
+  }
+  for(var i=1; i <= exponent; i++) {
+    result = result * base;
+  }
+  return result;
+}
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0)
+  return -1;
+
+  else if (num == 0)
+  return 1;
+
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
